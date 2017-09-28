b081c39f8466a75845df9c5affb9015f4a40ddc6
Ivan Yelizariev <yelizariev@it-projects.info>
2017-09-28 22:37:37 +0000

    [CI] run unittests with checkout workflow

---
+++ website_sale_require_login/tests/test_checkout.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import odoo.tests
+
+
+@odoo.tests.common.at_install(False)
+@odoo.tests.common.post_install(True)
+class TestUi(odoo.tests.HttpCase):
+    # keep test numbering from module website_sale. Test 01 is not needed in this module
+    def test_02_admin_checkout(self):
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product.ready", login="admin")
+
+    def test_03_demo_checkout(self):
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product.ready", login="demo")
+
+    def test_03_public_checkout(self):
+        self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product.ready")
